287122c0664847a172a93fc40354c964d1e3f381
Avinash96-gthb <avinash.a2k4@gmail.com>
2025-06-25 09:08:22 +0530

    Initial Commit

---
+++ CMDTests/CMDTests.swift
@@ -0,0 +1,17 @@
+//
+//  CMDTests.swift
+//  CMDTests
+//
+//  Created by A Avinash Chidambaram on 25/06/25.
+//
+
+import Testing
+@testable import CMD
+
+struct CMDTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
